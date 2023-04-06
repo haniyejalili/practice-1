2fce6a17e0495c3c294de1f744192c50febd248d
haniye jalili <haniyehjalili@gmail.com>
2023-04-06 01:42:58 -0700

    new

--- averge.py
+++ averge.py
@@ -3,14 +3,19 @@ family_name = input("please insert student last neme ")
 grade1 = float(input(" first lesson: "))
 grade2 = float(input(" second lesson: "))
 grade3 = float(input(" third lesson: "))
-
+print (name)
+print (family_name)
 
 average = (grade1 + grade2 + grade3) / 3
-if (grade1,grade2)<0:
-    print("error")
+if grade1 >20:
+    print("error number 1")
+if grade2 >20:
+    print("error number 2")
+if grade3 >20:
+    print("error number 3")
 
 if average >= 17:
-    print (name,"great.")
+    print ("great.")
 elif average >= 12<17:
     print("normal")
 else:
